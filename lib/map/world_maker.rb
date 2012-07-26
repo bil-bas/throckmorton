686bafad8103c5fe3a5af8a08190daa15688c8ac
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2012-07-26 14:07:51 +0100

    Pulled signed distance fields out into Ashton.

--- lib/map/world_maker.rb
+++ lib/map/world_maker.rb
@@ -9,8 +9,6 @@ module Game
     MAX_NAVIGATION_DISTANCE = SPAWN_SPACING
     DISTANCE_FIELD_SCALE = 1.0
 
-    ZERO_DISTANCE = 128 # color channel containing 0 => -128, 128 => 0, 255 => +127
-
     def initialize(map_texture, shadow_casters, seed)
       @map_texture = map_texture
       @rng = Random.new seed
@@ -25,39 +23,23 @@ module Game
         end
       end
 
-      $window.scale DISTANCE_FIELD_SCALE do
-        @signed_distance_field.draw 0, 0, 0, blend: :add
-      end
+      @signed_distance_field.draw 0, 0, 0
       @recording.draw 0, 0, 0
     end
 
     def position_clear?(x, y, radius)
-      clear_distance(x, y) >= radius
+      @signed_distance_field.position_clear? x, y, radius
     end
 
     def clear_distance(x, y)
-      # Could be checking any of red/blue/green.
-      @signed_distance_field.red(x / DISTANCE_FIELD_SCALE, y / DISTANCE_FIELD_SCALE) - ZERO_DISTANCE
+      @signed_distance_field.clear_distance x, y
     end
 
     # Nodes indicate the distance from themselves to a blockage. 0 if the node is in scenery.
     def generate_navigation_nodes(shadow_casters)
       t = Time.now
 
-      shader = Ashton::Shader.new fragment: fragment_shader("signed_distance_field"), uniforms: {
-          step_size: NAVIGATION_SPACING,
-          max_distance: MAX_NAVIGATION_DISTANCE,
-          texture_size: [shadow_casters.width, shadow_casters.height].map(&:to_f),
-      }
-
-      @signed_distance_field = Ashton::Framebuffer.new shadow_casters.width, shadow_casters.height
-      shader.use do
-        @signed_distance_field.render do
-          $window.scale 1.0 / DISTANCE_FIELD_SCALE do
-            shadow_casters.draw 0, 0, 0
-          end
-        end
-      end
+      @signed_distance_field = Ashton::SignedDistanceField.new shadow_casters, MAX_NAVIGATION_DISTANCE
 
       info "Navigation nodes plotted out in #{((Time.now - t).to_f * 1000).to_i}ms"
     end
@@ -68,7 +50,7 @@ module Game
 
       (0...@map_texture.width).step(SPAWN_SPACING) do |x|
         (0...@map_texture.height).step(SPAWN_SPACING) do |y|
-          @spawn_nodes << [x, y] if position_clear? x, y, SPAWN_MARGIN
+          @spawn_nodes << [x, y] if @signed_distance_field.position_clear? x, y, SPAWN_MARGIN
         end
       end
 
